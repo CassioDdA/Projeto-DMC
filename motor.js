742657871d6466953b30dcd7ba771768b5adc3c6
Cássio D <cassioddev@gmail.com>
2022-12-16 16:45:20 -0300

    adicionado o Projeto site

---
+++ motor.js
@@ -0,0 +1,14 @@
+var nome = document.getElementById('nome');
+
+var user = document.getElementById('user');
+
+var email = document.getElementById('email');
+
+var senha = document.getElementById('senha');
+
+function enviar(){
+    alert(nome.value);
+    alert(user.value);
+    alert(email.value);
+    alert(senha.value);
+}
